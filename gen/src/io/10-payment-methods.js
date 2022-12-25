ef77c788906652946e58c9855e962625741baf82
tierrif <tierriff@gmail.com>
2022-12-25 12:51:48 +0000

    Add housing categories.

--- gen/src/io/10-payment-methods.js
+++ gen/src/io/10-payment-methods.js
@@ -1,6 +1,6 @@
 export const multithread = true
 
-export const enabled = true
+export const enabled = false
 
 export const iterableDataStatement = `SELECT siteUserId FROM SiteUser WHERE isHost = 1`
 
---
+++ gen/src/io/11-housing-categories.js
@@ -0,0 +1,64 @@
+import { loremIpsum } from 'lorem-ipsum'
+
+export const multithread = true
+
+export const enabled = true
+
+export const iterableDataStatement = `SELECT housingId FROM Housing`
+
+export const iterableDataPrimaryKey = 'housingId'
+
+const categories = [
+  'Private Rooms',
+  'Entire Home',
+  'Unique Spaces',
+  'Shared Rooms',
+  'Housings with Pools',
+  'Luxury Homes',
+  'Vacation Rentals',
+  'Experiences',
+]
+
+export const insertSinglethread = async (mssql, pool) => {
+  const ps = new mssql.PreparedStatement(pool)
+  ps.input('name', mssql.NVarChar(32))
+  ps.input('description', mssql.NVarChar(128))
+  await ps.prepare('INSERT INTO Category ([name], [description]) VALUES (@name, @description)')
+
+  for (const category of categories) {
+    await ps.execute({
+      name: category,
+      description: loremIpsum({ count: 1, units: 'sentences', sentenceLowerBound: 3, sentenceUpperBound: 7 })
+    })
+  }
+
+  ps.unprepare()
+}
+
+export const insert = async (mssql, pool, housingId) => {
+  const ps = new mssql.PreparedStatement(pool)
+  ps.input('housingId', mssql.BigInt)
+  ps.input('categoryId', mssql.Int)
+  await ps.prepare(`INSERT INTO HousingCategory (housingId, categoryId)
+    VALUES (@housingId, @categoryId)`)
+
+  // Máximo de 3 categorias por alojamento.
+  const maxLength = Math.floor(Math.random() * 3) + 1
+  const insertedIds = []
+  for (let i = 0; i < maxLength; i++) {
+    const categoryId = Math.floor(Math.random() * categories.length) + 1
+    if (insertedIds.includes(categoryId)) {
+      --i
+      continue
+    }
+    
+    await ps.execute({
+      housingId,
+      categoryId
+    })
+
+    insertedIds.push(categoryId)
+  }
+
+  ps.unprepare()
+}
